ce489e76501e46c84a2f32f4143eaa27073c1623
VanekChris <VanekChris@proton.me>
2025-05-05 16:20:23 +0900

    main.py
    The main file to launch the game from.

--- main.py
+++ main.py
@@ -22,21 +22,3 @@ def main():
 
 if __name__=="__main__":
     main()
-
-"""
-created assets ✅
-dealt cards ✅
-dealt coins ✅
-Displayed coins ✅    
-Displayed Deck ✅
-Make turns for each player, income, foreign aid ✅
-Steal works ✅
-ensure that each turn for the player visibly ends before the next one ✅
-Assasinate works ✅
-coup works ✅
-Ensure that lost cards remain on screen. Maybe at the top. ✅
-
-challenge function(s)  card names seem to be a problem.
-declare winner
-restart game
-"""
